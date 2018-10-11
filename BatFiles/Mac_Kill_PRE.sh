320cc4fdd0eacbf8f3feb7e895ef0385e63724d8
Nidhi Bhushan <nidhi.bhushan123@gmail.com>
2018-10-11 10:25:55 +0530

    minor bat file fix

--- BatFiles/Mac_Kill_PRE.sh
+++ BatFiles/Mac_Kill_PRE.sh
@@ -1,4 +1,4 @@
 killall 'Adobe Premiere Elements 2019'
 killall 'Adobe Premiere Elements'
 killall 'Elements Auto Creator 17'
-rm -rf '~/Library/Application Support/Adobe/Premiere Elements/17.0/Adobe Premiere Elements Prefs'
+rm -rf '/Users/nbhushan/Library/Application Support/Adobe/Premiere Elements/17.0/Adobe Premiere Elements Prefs'
